80632b3d5f2fc96982c6a76213be0d1830655462
John Freddy <john.freddy.belalcazar@correounivalle.edu.co>
2023-04-26 13:40:08 -0500

    Terminado

--- src/main/scala/Huffman/package.scala
+++ src/main/scala/Huffman/package.scala
@@ -81,4 +81,78 @@ package object Huffman {
     else
       hastaQue(listaUnitaria,combinar)(listaDeHojasOrdenadas(ocurrencias(cars))).head
   }
+
+  type Bit = Int
+
+  def decodificar(arbol: ArbolH, bits: List[Bit]): List[Char] = {
+    def seleccionBit(nodo: Nodo, bits: List[Bit]): (ArbolH, List[Bit]) = bits match {
+      case 0 :: x => (nodo.izq, x)
+      case 1 :: x => (nodo.der, x)
+      case _ => throw new Error("Argumento no binario.")
+    }
+
+    def decodCar(arbol: ArbolH, bits: List[Bit]): (Char, List[Bit]) = arbol match {
+      case Nodo(izq, der, cars, peso) => {
+        val (nArbol, nBits) = seleccionBit(Nodo(izq, der, cars, peso), bits)
+        val (car, fBits) = decodCar(nArbol, nBits)
+        (car, fBits)
+      }
+      case Hoja(car, _) => (car, bits)
+    }
+
+    def decodMsj(arbol: ArbolH, bits: List[Bit], cars: List[Char]): List[Char] = bits match {
+      case Nil => cars
+      case _ => {
+        val (car, nBits) = decodCar(arbol, bits)
+        decodMsj(arbol, nBits, cars ::: (car :: Nil))
+      }
+    }
+
+    decodMsj(arbol, bits, Nil)
+  }
+
+  def codificar(arbol: ArbolH)(texto: List[Char]): List[Bit] = {
+
+    def codCar(arbol: ArbolH, car: Char, bits: List[Bit]): List[Bit] = arbol match {
+      case Hoja(_, _) => bits
+      case Nodo(izq, der, _, _) => {
+        if (cars(izq).contains(car)) codCar(izq, car, bits ::: (0 :: Nil))
+        else codCar(der, car, bits ::: (1 :: Nil))
+      }
+    }
+
+    def codMsj(arbol: ArbolH, texto: List[Char], bits: List[Bit]): List[Bit] = texto match {
+      case Nil => bits
+      case x :: xs => codMsj(arbol, xs, bits ::: codCar(arbol, x, Nil))
+    }
+
+    codMsj(arbol, texto, Nil)
+  }
+
+  type TablaCodigos = List[(Char, List[Bit])]
+
+  def codigoEnBits(tabla: TablaCodigos)(car: Char): List[Bit] = tabla match {
+    case Nil => Nil
+    case x :: xs => if (x._1 == car) x._2 else codigoEnBits(xs)(car)
+  }
+
+  def mezclarTablasDeCodigos(a: TablaCodigos, b: TablaCodigos): TablaCodigos = {
+    a ::: b
+  }
+
+  def convertir(arbol: ArbolH): TablaCodigos = arbol match {
+    case Nodo(izq, der, _, _) => mezclarTablasDeCodigos(convertir(izq).map(x => (x._1, 0 :: x._2)), convertir(der).map(x => (x._1, 1 :: x._2)))
+    case Hoja(car, _) => (car, Nil) :: Nil
+  }
+
+  def codificarRapido(arbol: ArbolH)(texto: List[Char]): List[Bit] = {
+    val tabla = convertir(arbol)
+
+    def codMsj(cod: Char => List[Bit], texto: List[Char], bits: List[Bit]): List[Bit] = texto match {
+      case Nil => bits
+      case x :: xs => cod(x) ::: codMsj(cod, xs, bits)
+    }
+
+    codMsj(codigoEnBits(tabla), texto, Nil)
+  }
 }
